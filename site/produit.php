fc186ab76805428ad5051b0c9978c474f30bcac2
adifuria <an.difuria@gmail.com>
2020-05-25 14:58:50 +0200

    projetUf

--- site/produit.php
+++ site/produit.php
@@ -1,8 +1,8 @@
-<?php include("header.php"); ?>
+﻿<?php include("header.php"); ?>
 <h1 class="titre">Votre produit</h1>
 <div class="container row">
     <div class="col col-4">
-        <img src="css/pikachu.png" heigh="auto" width="100%">
+        <img src="css/produit.png" heigh="auto" width="100%">
     </div>
 
     <?php
@@ -16,11 +16,11 @@
     
         foreach ($produits as $produit) { 
             echo '<div class="col col-auto">';
-            echo 'Nom:' . $produit->nom . '<br>';
-            echo 'Description: ' . $produit->description . '<br>';
-            echo 'Prix: ' . $produit->prix . '<br>';
-            echo 'Stock: ' . $produit->stock . '<br>';
-            echo 'commentaire: ' . $produit->commentaire . '<br>';
+            echo 'Nom : ' . $produit->nom . '<br>';
+            echo 'Description : ' . $produit->description . '<br>';
+            echo 'Prix : ' . $produit->prix . '<br>';
+            echo 'Stock : ' . $produit->stock . '<br>';
+            echo 'commentaire : ' . $produit->commentaire . '<br>';
             echo '<form method="get" action="creation_facture.php" class="form-inline my-2 my-lg-0">';
             echo '  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Acheter</button>';
             echo '</form>';
